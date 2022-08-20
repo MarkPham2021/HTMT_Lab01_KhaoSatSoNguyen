d12e85338338b885f429c7ebbfee760dbdb012c7
Mark Pham <pham.manh.cuong.sg@gmail.com>
2022-08-20 09:17:28 +0700

    push on github

--- Lab01_KhaoSatSoNguyen/KhaoSatSoNguyen/ChuyenDoiCoSo.cs
+++ Lab01_KhaoSatSoNguyen/KhaoSatSoNguyen/ChuyenDoiCoSo.cs
@@ -29,7 +29,57 @@ namespace KhaoSatSoNguyen
             }            
             return kq;
         }
+        public static string DectoHex(int n)
+        {
+            var kq = "";
 
-
+            if (n >= 0)
+            {
+                var tempstk = new Stack<int>();
+                int du = 0;
+                do
+                {
+                    du = n % 16;
+                    tempstk.Push(du);
+                    n = n / 16;
+                } while (n > 0);
+                
+                while (tempstk.Count > 0)
+                {
+                    int c = tempstk.Pop();
+                    switch (c)
+                    {
+                        case 10:
+                            kq += "A";
+                            break;
+                        case 11:
+                            kq += "B";
+                            break;
+                        case 12:
+                            kq += "C";
+                            break;
+                        case 13:
+                            kq += "D";
+                            break;
+                        case 14:
+                            kq += "E";
+                            break;
+                        case 15:
+                            kq += "F";
+                            break;
+                        default:
+                            kq += c.ToString();
+                            break;
+                    }                     
+                }
+            }
+            return kq;
+        }
+        public static int BintoDec(string bit)
+        {
+            int kq = 0;
+            
+            return kq;
+        }
     }
 }
--- Lab01_KhaoSatSoNguyen/KhaoSatSoNguyen/Program.cs
+++ Lab01_KhaoSatSoNguyen/KhaoSatSoNguyen/Program.cs
@@ -6,16 +6,22 @@ namespace KhaoSatSoNguyen
     {
         static void Main(string[] args)
         {
-            //int n = 30;
-            //Console.WriteLine($"{n} (10) = {ChuyenDoiCoSo.DectoBin(n)} (2).");
-            //string bit1 = "00000110", bit2 = "10000110";
-            //Console.WriteLine($"{bit1} + {bit2} = {CacPhepToanNhiPhan.PhepCong(bit1, bit2)}");
+            int n = 252;
+            string bit = "1001";
+            Console.WriteLine("---------------------------------------------------------------------------------------");
+            Console.WriteLine("Bai 1: Chuyen Doi Co So doi voi so nguyen duong: ");
+            Console.WriteLine($"{n} (10) = {ChuyenDoiCoSo.DectoBin(n)} (2).");
+            Console.WriteLine($"{n} (10) = {ChuyenDoiCoSo.DectoHex(n)} (16).");
+            Console.WriteLine("---------------------------------------------------------------------------------------");
+            Console.WriteLine("Bai 2: Thuc hien cac phep tinh voi chuoi nhi phan 8 bit so Nguyen co dau dang bu 2: ");
             string bit1 = "11101001", bit2 = "00000101"; string sodu = "";
-            Console.WriteLine($"so doi cua {bit1} = {CacPhepToanNhiPhan.LaySoDoi(bit1)}");
+            
+            Console.WriteLine($"{bit1} + {bit2} = {CacPhepToanNhiPhan.PhepCong(bit1, bit2)}");
             Console.WriteLine($"{bit1} - {bit2} = {CacPhepToanNhiPhan.PhepTru(bit1, bit2)}");
             Console.WriteLine($"{bit1} * {bit2} = {CacPhepToanNhiPhan.PhepNhan(bit1, bit2)}");
             Console.WriteLine($"{bit1} / {bit2} = {CacPhepToanNhiPhan.PhepChia(bit1, bit2, out sodu)} du: {sodu}");
             Console.WriteLine($"{bit1} / {bit2} = {CacPhepToanNhiPhan.PhepChiaM(bit1, bit2, out sodu)} du: {sodu}");
+            Console.WriteLine("---------------------------------------------------------------------------------------");
         }
     }
 }
